05f13c18f7239d7ada01f79201ce3d39aa1fb039
YurkinES <6eJlomop@inbox.ru>
2023-03-10 00:29:14 +0300

    Start stage

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static int[] generateRandomArray(){
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int totalWaste = 
+    }
+}

075f373bbd34b7738a9d3ffa53c9827631c9275b
YurkinES <6eJlomop@inbox.ru>
2023-03-10 00:47:20 +0300

    Second stage

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static int[] generateRandomArray(){
         java.util.Random random = new java.util.Random();
@@ -12,6 +15,25 @@ public class Main {
     }
     public static void task1() {
         int[] arr = generateRandomArray();
-        int totalWaste = 
+        int totalWaste = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalWaste = totalWaste + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила: " + totalWaste + " рублей");
     }
-}
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int minWaste = 200000000;
+        int maxWaste = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(maxWaste < arr[i]){
+                maxWaste = arr[i];
+            }
+            if (minWaste > arr[i]) {
+                minWaste = arr[i];
+            }
+            }
+        System.out.println("Минимальная сумма трат за день составила: " + minWaste + " рублей");
+        System.out.println("Максимальная сумма трат за день составила: " + maxWaste + " рублей");
+        }
+    }
